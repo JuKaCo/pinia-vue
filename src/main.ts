d9f07abc4ead6f43a4e3635e213e090a5257a7c1
Juan Carlos Condori <jucaco89@gmail.com>
2022-03-24 16:56:44 -0400

    adding primevue

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                //core css
+import 'primeicons/primeicons.css'                         //icons
 
 createApp(App)
     .use (createPinia())
     .use( router  )
+    .use ( PrimeVue )
     .mount('#app')
--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -2,11 +2,13 @@
     <h1>
         Counter View {{counterStore.counter}}
     </h1>
-    <button @click="counterStore.incrementBy(1)">+1</button>
-    <button @click="counterStore.incrementBy(2)">+2</button>
-    <button @click="counterStore.incrementBy(5)">+5</button>
+    <Button @click="counterStore.incrementBy(1)" label="+1" class="p-button-raised p-button-outlined p-button-secondary" />
+    <Button @click="counterStore.incrementBy(2)" label="+2" class="p-button-raised p-button-outlined p-button-success" />
+    <Button @click="counterStore.incrementBy(5)" label="+5" class="p-button-raised p-button-outlined p-button-help" />
+
 </template>
 <script lang="ts" setup>
-import {useCounterStore} from '../stores/counter'
+import {useCounterStore} from '../stores/counter';
+import Button from 'primevue/button';
 const counterStore = useCounterStore();
 </script>
